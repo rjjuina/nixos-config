7d8cff7809c71477b654f889ec58b9c8b46c4c23
halston <haslton@nixos.orbstack>
2025-12-23 17:56:37 +0800

    enable xserver for ovc-client development

--- hosts/nixos-fusion/configuration.nix
+++ hosts/nixos-fusion/configuration.nix
@@ -1,4 +1,9 @@
-{ config, pkgs, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
 {
   imports = [
@@ -6,11 +11,39 @@
     ../../modules/packages/common.nix
   ];
 
+  # Enable X11 and GNOME
+  networking.networkmanager.enable = true;
+  # networking.interfaces.ens160.useDHCP = true;
+
+  # Force use of reliable public DNS servers
+  networking.nameservers = [
+    "8.8.8.8"
+    "1.1.1.1"
+    "8.8.4.4"
+  ];
+
+  # Prevent NetworkManager from overwriting DNS settings
+  networking.networkmanager.dns = "none";
+
+  services.xserver.enable = true;
+  services = {
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
+  };
+
   environment.systemPackages = with pkgs; [
     nodejs_24
     claude-code
     stow
     neovim
+    openssl
+
+    # Development packages for GTK4/Rust
+    gtk4
+    libadwaita
+    pkg-config
+    gcc
+    rustup
   ];
 
   nixpkgs.config.allowUnfree = true;
